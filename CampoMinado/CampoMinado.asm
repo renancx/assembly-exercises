dcdfb95e97f2a570f82bb057b4f8b01541120c6c
Renan <renan.lowe@hotmail.com>
2022-07-11 12:57:53 -0300

    first part

--- CampoMinado/CampoMinado.asm
+++ CampoMinado/CampoMinado.asm
@@ -1,157 +1,152 @@
-###############################################################
-#A função insere_bombas necessita os seguintes rótulos
-# e espaços de memória reservados
-		.data
-campo:			.space		576   # esta versão suporta campo de até 12 x 12 posições de memória
-salva_S0:		.word		0
-salva_ra:		.word		0
-salva_ra1:		.word		0
+.data
+	
+OitoPorOito:                    # matriz 8x8 que será vista pelo usuario
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1
+	
+DezPorDez:                    # matriz 10x10 que será vista
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	
+DozePorDoze:                    # matriz 12x12 que será vista
 
-#########################################################
-# necessário em caso de debug da funcao
-#espaco:			.asciz		" "
-#dois_pontos:		.asciz		": "
-#nova_linha:		.asciz		"\n"
-#posicao:		.asciz		"\nPosicao bomba "
-#########################################################
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+	.word   -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+		  
 
-	.text
 
-########################################################
-# função main somente para teste. Não deve ser copiada 
-#  para o código do trabalho
+SelOito:  .string		"\n1) Selecionar tamanho 8x8"
+SelDez:	  .string		"\n2) Selecionar tamanho 10x10"
+SelDoze:  .string		"\n3) Selecionar tamanho 12x12\n"
+barra:	  .string		"|"
+branco:	  .string		" "
+linha:	  .string		"\n"
+
+ .text
+
 main:
-	la 	a0, campo	# endereço inicial do campo
-	addi	a1, zero, 12	# quantidade de linhas do campo
-	jal 	INSERE_BOMBA
-	nop
-fim:    addi	a7, zero, 10	# encerra o programa
+
+        li s1, 1
+        li s2,2
+        li s3,3
+	la  a0, SelOito
+	li  a7, 4 
 	ecall
-
-
-############################################################
-# A função insere_bombas está implementada abaixo.
-# Todo o código a seguir, mais os rotulos acima deve 
-# ser colocado junto ao código do trabalho de campo minado
-# entrada: a0 endereço de memoria do inicio da matriz campo
-#          a1 quantidade de linhas da matriz campo
-# saida: nao retorna nada
-############################################################
-#     Algoritmo	
-#
-#  Salva registradores
-#  Carrega numero de sementes sorteadas = 15
-#  Le semente para função de numero pseudo randomico
-#  while (bombas < x) 
-#     sorteia linha
-#     sorteia coluna
-#     le posição pos = (L * tam + C) * 4
-#     if(pos != 9)
-#    	  grava posicao pos = 9
-#     bombas++  
-#
-############################################################
-INSERE_BOMBA:
-		la	t0, salva_S0
-		sw  	s0, 0 (t0)		# salva conteudo de s0 na memoria
-		la	t0, salva_ra
-		sw  	ra, 0 (t0)		# salva conteudo de ra na memoria
+	la  a0, SelDez
+	li  a7, 4 
+	ecall
+	la  a0, SelDoze
+	li  a7, 4 
+	ecall
+	
+	li a7, 5
+    	ecall
+        
+        beq a0,s1,mostraOito
+    	beq a0,s2,mostraDez
+    	beq a0,s3,mostraDoze
+    	
+mostraOito:
+	addi a1,a1,8 #quando vai parar(linha e coluna)
+	inicio8:
+	addi t2,t2,0
+	addi t0,t2,0
+	la  a0, linha
+	li  a7, 4 
+	ecall
+	loop8: #imprime linha
+		beq a1,t0,loop28
+		la  a0, barra
+		li  a7, 4 
+		ecall
+	
+		la  a0, branco
+		li  a7, 4 
+		ecall
+		addi t0,t0,1
 		
-		add 	t0, zero, a0		# salva a0 em t0 - endereço da matriz campo
-		add 	t1, zero, a1		# salva a1 em t1 - quantidade de linhas 
+		j loop8
+	loop28:
+		addi t1,t1,1
+		beq t1,a1,end
+		j inicio8
 
-QTD_BOMBAS:
-		addi 	t2, zero, 15 		# seta para 15 bombas	
-		add 	t3, zero, zero 	# inicia contador de bombas com 0
-		addi 	a7, zero, 30 		# ecall 30 pega o tempo do sistema em milisegundos (usado como semente
-		ecall 				
-		add 	a1, zero, a0		# coloca a semente em a1
-INICIO_LACO:
-		beq 	t2, t3, FIM_LACO
-		add 	a0, zero, t1 		# carrega limite para %	(resto da divisão)
-		jal 	PSEUDO_RAND
-		add 	t4, zero, a0		# pega linha sorteada e coloca em t4
-		add 	a0, zero, t1 		# carrega limite para % (resto da divisão)
-   		jal 	PSEUDO_RAND
-		add 	t5, zero, a0		# pega coluna sorteada e coloca em t5
-
-###############################################################################
-# imprime valores na tela (para debug somente) - retirar comentarios para ver
-#	
-#		li	a7, 4		# mostra texto "Posicao: "
-#		la	a0, posicao
-#		ecall
-#		
-#		li	a7, 1		
-#		add 	a0, zero, t3 	# imprime quantidade ja sorteada
-#		ecall		
-#
-#		li	a7, 4		# imrpime :
-#		la	a0, dois_pontos
-#		ecall
-#
-#		li	a7, 1
-#		add 	a0, zero, t4 	# imprime a linha sorteada	
-#		ecall
-#		
-#		li	a7, 4		# imrpime espaço
-#		la	a0, espaco
-#		ecall	
-#			
-#		li	a7, 1
-#		add 	a0, zero, t5 	# imprime coluna sorteada
-#		ecall
-#		
-##########################################################################	
-
-LE_POSICAO:	
-		mul  	t4, t4, t1
-		add  	t4, t4, t5  		# calcula (L * tam) + C
-		add  	t4, t4, t4  		# multiplica por 2
-		add  	t4, t4, t4  		# multiplica por 4
-		add  	t4, t4, t0  		# calcula Base + deslocamento
-		lw   	t5, 0(t4)   		# Le posicao de memoria LxC
-VERIFICA_BOMBA:		
-		addi 	t6, zero, 9		# se posição sorteada já possui bomba
-		beq  	t5, t6, PULA_ATRIB	# pula atribuição 
-		sw   	t6, 0(t4)		# senão coloca 9 (bomba) na posição
-		addi 	t3, t3, 1		# incrementa quantidade de bombas sorteadas
-PULA_ATRIB:
-		j	INICIO_LACO
-
-FIM_LACO:					# recupera registradores salvos
-		la	t0, salva_S0
-		lw  	s0, 0(t0)		# recupera conteudo de s0 da memória
-		la	t0, salva_ra
-		lw  	ra, 0(t0)		# recupera conteudo de ra da memória		
-		jr 	ra			# retorna para funcao que fez a chamada
+mostraDez:
+	addi a1,a1,10 #quando vai parar(linha e coluna)
+	inicio10:
+	addi t2,t2,0
+	addi t0,t2,0
+	la  a0, linha
+	li  a7, 4 
+	ecall
+	loop10: #imprime linha
+		beq a1,t0,loop210
+		la  a0, barra
+		li  a7, 4 
+		ecall
+	
+		la  a0, branco
+		li  a7, 4 
+		ecall
+		addi t0,t0,1
 		
-##################################################################
-# PSEUDO_RAND
-# função que gera um número pseudo-randomico que será
-# usado para obter a posição da linha e coluna na matriz
-# entrada: a0 valor máximo do resultado menos 1 
-#             (exemplo: a0 = 8 resultado entre 0 e 7)
-#          a1 para o número pseudo randomico 
-# saida: a0 valor pseudo randomico gerado
-#################################################################
-#int rand1(int lim, int semente) {
-#  static long a = semente; 
-#  a = (a * 125) % 2796203; 
-#  return (|a % lim|); 
-# }  
-
-PSEUDO_RAND:
-		addi t6, zero, 125  		# carrega constante t6 = 125
-		lui  t5, 682			# carrega constante t5 = 2796203
-		addi t5, t5, 1697 		# 
-		addi t5, t5, 1034 		# 	
-		mul  a1, a1, t6			# a = a * 125
-		rem  a1, a1, t5			# a = a % 2796203
-		rem  a0, a1, a0			# a % lim
-		bge  a0, zero, EH_POSITIVO  	# testa se valor eh positivo
-		addi s2, zero, -1           	# caso não 
-		mul  a0, a0, s2		    	# transforma em positivo
-EH_POSITIVO:	
-		ret				# retorna em a0 o valor obtido
-############################################################################
+		j loop10
+	loop210:
+		addi t1,t1,1
+		beq t1,a1,end
+		j inicio10
+		
+mostraDoze:
+	addi a1,a1,12 #quando vai parar(linha e coluna)
+	inicio12:
+	addi t2,t2,0
+	addi t0,t2,0
+	la  a0, linha
+	li  a7, 4 
+	ecall
+	loop12: #imprime linha
+		beq a1,t0,loop212
+		la  a0, barra
+		li  a7, 4 
+		ecall
+	
+		la  a0, branco
+		li  a7, 4 
+		ecall
+		addi t0,t0,1
+		
+		j loop12
+	loop212:
+		addi t1,t1,1
+		beq t1,a1,end
+		j inicio12
+		
+end: 
+	nop
